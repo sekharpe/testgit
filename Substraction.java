bd40164663bd555bd2580952223807a235d991d5
sekharpe <101968635+sekharpe@users.noreply.github.com>
2022-03-20 15:52:53 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=15,b=10,c;
+      c=a-b;
+      System.out.println("the difference between '&a' and '&b' is " +c);
+	}
+
+}
